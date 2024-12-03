c7b9260a54003fb3fb04996cb8ed9987f3ab99bb
MirkoDD <mirkodidio111@gmail.com>
2024-12-03 15:44:47 +0100

    added:first_concept_of_program

---
+++ exercise-1/javascript.js
@@ -0,0 +1,21 @@
+/* Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Non è necessario provvedere alla validazione delle email.
+NON POTETE UTILIZZARE NESSUN METODO COME:
+- includes
+- indexOf
+- find
+- search
+- ecc... 
+*/
+
+/*
+1 - chiedo l'email all'utente e la salvo
+2 - verifico SE l'email inserita corrisponde ad almeno una di quelle già presenti nella lista
+  - SE si può proseguire nel party
+  - ALTRIMENTI gli chiedo di riprovare con una mail diversa
+*/
+
+
